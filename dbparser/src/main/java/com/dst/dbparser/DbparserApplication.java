42cf584a18789a683138ea88d4457a792f330e29
Vlad Pr <priselkov@tm10.ru>
2020-10-06 09:07:10 +0500

    some changes

--- dbparser/src/main/java/com/dst/dbparser/DbparserApplication.java
+++ dbparser/src/main/java/com/dst/dbparser/DbparserApplication.java
@@ -15,7 +15,7 @@ import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
-import java.util.List;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 @SpringBootApplication
@@ -24,9 +24,7 @@ public class DbparserApplication {
     public static void main(String[] args) {
         SpringApplication.run(DbparserApplication.class, args);
 
-//        MongoOperations mongoOpsRaw = new MongoTemplate(new SimpleMongoClientDatabaseFactory(MongoClients.create(), "wheel_loaders"));
         MongoOperations mongoOpsRaw = new MongoTemplate(new SimpleMongoClientDatabaseFactory(MongoClients.create("mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false"), "wheel_loaders"));
-//        MongoOperations mongoOpsParsed = new MongoTemplate(new SimpleMongoClientDatabaseFactory(MongoClients.create(), "wl_parsed"));
         MongoOperations mongoOpsParsed = new MongoTemplate(new SimpleMongoClientDatabaseFactory(MongoClients.create("mongodb://root:root@localhost:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false"), "wl_parsed"));
 
         while (true) {
@@ -37,21 +35,23 @@ public class DbparserApplication {
                     mongoOpsParsed.insertAll((mongoOpsRaw.findAll(LocarusDataField.class, loaderCollection).stream().map(DataParser::parseLocarusDataField)).collect(Collectors.toList()));
                     MongoNamespace namespace = new MongoNamespace("wl_parsed", loaderCollection);
                     mongoOpsParsed.getCollection(ParsedEntity.class.getSimpleName().replace(ParsedEntity.class.getSimpleName().charAt(0), ParsedEntity.class.getSimpleName().toLowerCase().charAt(0))).renameCollection(namespace);
+                    System.out.println("Created collection: " + loaderCollection);
                 }
                 //Коллекция есть
                 else {
-                    System.out.println("Collection exists");
+                    System.out.println("Collection: " + loaderCollection);
                     Query query = new Query().with(Sort.by(Sort.Direction.DESC, "time")).limit(1);
-                    Time lastTimeRaw = mongoOpsRaw.find(query, LocarusDataField.class, loaderCollection).get(0).getTime();
-                    System.out.println("lastTimeRaw: " + lastTimeRaw);
-                    /*Time*/String lastTimeParsed = mongoOpsParsed.find(query, ParsedEntity.class, loaderCollection).get(0).getTime();
-                    System.out.println("lastTimeParsed: " + lastTimeParsed);
-//                System.out.println(lastTimeRaw.compareTo(lastTimeParsed));
+                    String lastTimeRaw = mongoOpsRaw.find(query, LocarusDataField.class, loaderCollection).get(0).getTime().getValue();
+                    String lastTimeParsed = mongoOpsParsed.find(query, ParsedEntity.class, loaderCollection).get(0).getTime();
                     //Время последней записи не совпадает
-                    if (!lastTimeRaw.getValue().equals(lastTimeParsed/*.getValue()*/)) {
-                        Query queryGreater = new Query().addCriteria(Criteria.where("time").gt(lastTimeParsed));
-                        mongoOpsParsed.insert(mongoOpsRaw.find(queryGreater, LocarusDataField.class, loaderCollection).stream().map(DataParser::parseLocarusDataField).collect(Collectors.toList()), loaderCollection);
-                        System.out.println("Need to insert " + mongoOpsRaw.find(queryGreater, LocarusDataField.class, loaderCollection).size() + " documents.");
+                    if (!lastTimeRaw.equals(lastTimeParsed)) {
+                        Query queryGreater = new Query().addCriteria(Criteria.where("time.value").gt(lastTimeParsed));
+                        System.out.println(queryGreater);
+                        ArrayList<ParsedEntity> insd = (ArrayList<ParsedEntity>) mongoOpsParsed.insert(mongoOpsRaw.find(queryGreater, LocarusDataField.class, loaderCollection).stream().map(DataParser::parseLocarusDataField).collect(Collectors.toList()), loaderCollection);
+                        System.out.println("Inserted " + insd.size() + " documents.");
+                    }
+                    else {
+                        System.out.println("Synchronized at: " + lastTimeParsed);
                     }
                 }
             }
